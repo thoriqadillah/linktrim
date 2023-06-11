d6bc45a9422d60b562b5db4cc4c61820af09754e
thoriqadillah <thoriqadillah59@gmail.com>
2023-06-11 16:44:51 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(logger.New())
+
+	app.Listen(":8000")
+}
